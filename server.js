457b674fdcb34f5ee57e5ed28af013d8c56630d6
Karla Moreno <moreno_karla@live.com>
2017-11-06 18:51:39 -0600

    set up backend server

---
+++ server.js
@@ -0,0 +1,13 @@
+//dependencies
+const app = require('express');
+
+//setup
+//make calls to yelp api
+
+//routes
+// do setup stuff when accessed
+
+//start
+app.listen(3000, function(){
+  console.log("server listening on port 3000");
+});
