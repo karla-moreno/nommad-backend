92d2e43082807eb75451b55fe7e2cd2300d8232b
Karla Moreno <moreno_karla@live.com>
2017-11-07 06:36:09 -0600

    set up homepage route, server actually functional now

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 //dependencies
-const app = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const yelp = require('yelp-fusion');
+require('dotenv').config();
 
 //setup
-//make calls to yelp api
+const app = express();
+app.use(bodyParser.json());
 
-//routes
-// do setup stuff when accessed
+//get access token
+//TODO: plug in clientId and clientSecret via dotenv
+//TODO: add these env. var. to heroku as well
+// const token = yelp.accessToken(clientId, clientSecret).then(response => {
+//   console.log(response.jsonBody.access_token);
+// }).catch(e => {
+//   console.log(e);
+// });
+//
+// //
+// const client = yelp.client(token);
+
+//search using token
+// client.search({
+//   term:'Four Barrel Coffee',
+//   location: 'san francisco, ca'
+// }).then(response => {
+//   console.log(response.jsonBody.businesses[0].name);
+// }).catch(e => {
+//   console.log(e);
+// });
+
+//route
+//home page
+app.get('/', function(req, res){
+  res.send('howdy');
+})
+//call yelp API and return JSON
 
 //start
 app.listen(3000, function(){
