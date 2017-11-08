53e88b81e42eac2c56665e9f2f43bb0c8af85ca6
Karla Moreno <moreno_karla@live.com>
2017-11-08 11:47:58 -0600

    start test route

--- server.js
+++ server.js
@@ -65,6 +65,20 @@ app.get('/api', (req, res) => {
   });
 });
 
+// testing
+app.get('/test', (req, res) => {
+  yelpClientPromise.then(client => {
+    handleClientAction(res, client.search({
+      term:'foodtrucks',
+      location: 'austin',
+      radius: 40000
+    }));
+  }).catch(err => {
+    console.log(err);
+    res.status(500).send('error retrieving data from Yelp');
+  });
+});
+
 //start
 app.listen(PORT, function(){
   console.log(`server listening on ${PORT}`);
