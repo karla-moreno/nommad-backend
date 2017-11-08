129ab59822b00619c9119b0efd4d7a366599f752
Karla Moreno <moreno_karla@live.com>
2017-11-08 12:28:33 -0600

    testing test route

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/test', (req, res) => {
   yelpClientPromise.then(client => {
     handleClientAction(res, client.search({
       term:'foodtrucks',
-      location: 'austin',
+      location: location,
       radius: 40000
     }));
   }).catch(err => {
